a610c29a2a15afa81bc7a96f5dd32cf830a047b0
Pablo Orviz <orviz@ifca.unican.es>
2024-03-06 16:40:37 +0100

    Add pyproject.toml

--- step.py
+++ step.py
@@ -6,15 +6,15 @@ import argparse
 import json
 import logging
 import os
-import requests
 import sys
 
+import requests
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger('sqaaas-step-action')
+logger = logging.getLogger("sqaaas-step-action")
 
 
-TOOLING_URL = 'https://raw.githubusercontent.com/EOSC-synergy/sqaaas-tooling/release/2.0.0/tooling.json'
+TOOLING_URL = "https://raw.githubusercontent.com/EOSC-synergy/sqaaas-tooling/release/2.0.0/tooling.json"
 
 
 def get_tool_data(tool, lang):
@@ -24,13 +24,12 @@ def get_tool_data(tool, lang):
     tool -- the tool to get the tooling arguments from
     lang -- the language that the tool is mapped to
     """
-    logger.debug('Using SQAaaS tooling metadata: %s' % TOOLING_URL)
-    req = requests.get(
-        url=TOOLING_URL
-    )
+    logger.debug("Using SQAaaS tooling metadata: %s" % TOOLING_URL)
+    req = requests.get(url=TOOLING_URL)
     data = req.json()
 
-    return data['tools'][lang][tool]
+    return data["tools"][lang][tool]
+
 
 def get_tooling_args(args):
     """Format args indexed by tool id.
@@ -42,9 +41,12 @@ def get_tooling_args(args):
     tooling_args_after = {}
     for arg in tooling_args_before:
         try:
-            arg_id = arg.pop('id')
+            arg_id = arg.pop("id")
         except KeyError:
-            logger.debug('Ignoring argument <%s> as it does not provide the <id> property in SQAaaS tooling' % arg)
+            logger.debug(
+                "Ignoring argument <%s> as it does not provide the <id> property in SQAaaS tooling"
+                % arg
+            )
         else:
             tooling_args_after[arg_id] = arg
 
@@ -65,26 +67,27 @@ def get_envvar(envvar=None, prefix=None, ignore_envvars=[]):
         if envvar:
             envvars = {envvar: os.environ[envvar]}
         elif prefix:
-            envvars = dict([
-                (key, os.environ[key])
-                    for key in os.environ.keys() if key.startswith(prefix)
-            ])
+            envvars = dict(
+                [
+                    (key, os.environ[key])
+                    for key in os.environ.keys()
+                    if key.startswith(prefix)
+                ]
+            )
     except KeyError:
-        logger.error('Could not find environment variable: %s' % envvar)
+        logger.error("Could not find environment variable: %s" % envvar)
 
     if ignore_envvars:
-        logger.debug(
-            'Request to ignore environment variables: %s' % ignore_envvars
-        )
+        logger.debug("Request to ignore environment variables: %s" % ignore_envvars)
         for key in ignore_envvars:
-            logger.debug('Ignoring environment variable <%s>' % key)
+            logger.debug("Ignoring environment variable <%s>" % key)
             try:
                 del envvars[key]
             except KeyError as e:
-                logger.debug('Cannot ignore variable. Variable not set: %s' % key)
+                logger.debug("Cannot ignore variable. Variable not set: %s" % key)
         logger.debug(
-            'Resultant set of environment variables after ignoring '
-            'process: %s' % envvars
+            "Resultant set of environment variables after ignoring "
+            "process: %s" % envvars
         )
 
     return envvars
@@ -97,41 +100,35 @@ def generate_args_json(tooling_args):
     tooling_args -- a dict with the SQAaaS tooling data
     """
     input_envvars = get_envvar(
-        prefix='INPUT', ignore_envvars=[
-            'INPUT_TOOL', 'INPUT_NAME', 'INPUT_CONTAINER'
-        ]
+        prefix="INPUT", ignore_envvars=["INPUT_TOOL", "INPUT_NAME", "INPUT_CONTAINER"]
     )
 
     tooling_args_keys = tooling_args.keys()
     tool_args = []
     for arg_k, arg_v in input_envvars.items():
-        action_arg = arg_k.replace('INPUT_', '').lower()
+        action_arg = arg_k.replace("INPUT_", "").lower()
         if action_arg not in tooling_args_keys:
             logger.debug(
-                'Tool argument <%s> not in SQAaaS tooling: '
-                '(tooling args: %s)' % (action_arg, tooling_args_keys)
+                "Tool argument <%s> not in SQAaaS tooling: "
+                "(tooling args: %s)" % (action_arg, tooling_args_keys)
             )
         else:
             _arg = tooling_args[action_arg]
             logger.debug(
-                'Found matching tooling argument <%s>: %s' % (action_arg, _arg)
+                "Found matching tooling argument <%s>: %s" % (action_arg, _arg)
             )
             # If arg has 'repeatable=true' it might contain YAML multiline string
-            if _arg.get('repeatable', False):
-                arg_v = arg_v.split('\n')
+            if _arg.get("repeatable", False):
+                arg_v = arg_v.split("\n")
                 arg_v = list(filter(None, arg_v))
                 logger.debug(
-                    'Action input <%s> contains multiline value: %s' % (
-                        action_arg, arg_v
-                    )
+                    "Action input <%s> contains multiline value: %s"
+                    % (action_arg, arg_v)
                 )
             # Compose tool_args
-            _arg.update({
-                'id': action_arg,
-                'value': arg_v
-            })
+            _arg.update({"id": action_arg, "value": arg_v})
             tool_args.append(_arg)
-            logger.debug('Tracking tooling argument: %s' % tool_args)
+            logger.debug("Tracking tooling argument: %s" % tool_args)
 
     return tool_args
 
@@ -139,13 +136,11 @@ def generate_args_json(tooling_args):
 def generate_container_json():
     """Generate JSON payload corresponding to a Docker definition."""
     payload = {}
-    container = get_envvar(envvar='INPUT_CONTAINER')
+    container = get_envvar(envvar="INPUT_CONTAINER")
     if container:
-        container = container['INPUT_CONTAINER']
-        payload = {
-            'image': container
-        }
-        logger.debug('New \'docker\' payload: %s' % payload)
+        container = container["INPUT_CONTAINER"]
+        payload = {"image": container}
+        logger.debug("New 'docker' payload: %s" % payload)
 
     return payload
 
@@ -161,48 +156,52 @@ def generate_step_json(tool, lang):
     # container
     container_payload = generate_container_json()
     if container_payload:
-        payload['docker'] = container_payload
+        payload["docker"] = container_payload
     # args
-    args = payload.get('args', [])
+    args = payload.get("args", [])
     if args:
         tooling_args = get_tooling_args(args)
         args = generate_args_json(tooling_args)
-    payload['args'] = args
-    payload.update({
-        'name': tool,
-        'lang': lang,
-    })
+    payload["args"] = args
+    payload.update(
+        {
+            "name": tool,
+            "lang": lang,
+        }
+    )
 
     return payload
 
 
 if __name__ == "__main__":
-    action_tool = get_envvar(envvar='INPUT_TOOL')['INPUT_TOOL']
-    if action_tool in ['commands']:
-        lang = 'default'
-    elif action_tool in ['pytest', 'tox']:
-        lang = 'Python'
+    action_tool = get_envvar(envvar="INPUT_TOOL")["INPUT_TOOL"]
+    if action_tool in ["commands"]:
+        lang = "default"
+    elif action_tool in ["pytest", "tox"]:
+        lang = "Python"
     else:
-        logger.error('Tool <%s> not supported' % action_tool)
+        logger.error("Tool <%s> not supported" % action_tool)
         sys.exit(2)
 
     step_json = generate_step_json(action_tool, lang)
-    logger.info('Step definition (JSON format): %s' % step_json)
+    logger.info("Step definition (JSON format): %s" % step_json)
 
-    github_workspace = get_envvar(envvar='GITHUB_WORKSPACE')['GITHUB_WORKSPACE']
+    import sys
+
+    sys.exit(0)
+
+    github_workspace = get_envvar(envvar="GITHUB_WORKSPACE")["GITHUB_WORKSPACE"]
     if github_workspace:
-        logger.debug('Found GitHub workspace: %s' % github_workspace)
+        logger.debug("Found GitHub workspace: %s" % github_workspace)
     else:
         logger.error(
-            'Cannot store step definition: GitHub workspace not defined '
-            'through GITHUB_WORKSPACE variable'
+            "Cannot store step definition: GitHub workspace not defined "
+            "through GITHUB_WORKSPACE variable"
         )
         sys.exit(3)
-    action_name = get_envvar(envvar='INPUT_NAME')['INPUT_NAME']
-    step_file = '.'.join([action_name, 'json'])
+    action_name = get_envvar(envvar="INPUT_NAME")["INPUT_NAME"]
+    step_file = ".".join([action_name, "json"])
     step_file_abspath = os.path.join(github_workspace, step_file)
-    with open(step_file_abspath, 'w', encoding='utf-8') as f:
+    with open(step_file_abspath, "w", encoding="utf-8") as f:
         json.dump(step_json, f, ensure_ascii=False, indent=4)
-    logger.info(
-        'Step definition (JSON format) dumped to file: %s' % step_file_abspath
-    )
+    logger.info("Step definition (JSON format) dumped to file: %s" % step_file_abspath)
