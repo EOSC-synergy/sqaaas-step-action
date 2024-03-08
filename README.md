9f3e0bd4e3f4f2ed4eb3fda5dcf12f7f7ceb9254
Pablo Orviz <orviz@ifca.unican.es>
2024-03-08 10:56:18 +0100

    Compliance with REUSE

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 <!--
 SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project.
+SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
 
 SPDX-License-Identifier: GPL-3.0-only
 -->
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 # SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project.
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
