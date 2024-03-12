c33fe9561aae94d8f0f31a32f1f088e7eae2df47
Pablo Orviz <orviz@ifca.unican.es>
2024-03-12 11:04:34 +0100

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
+#!/bin/sh -l
+
 # SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project.
 # SPDX-FileContributor: 2017-2024 Pablo Orviz <orviz@ifca.unican.es>
 #
 # SPDX-License-Identifier: GPL-3.0-only
-
-#!/bin/sh -l
 
 python /usr/bin/step.py
